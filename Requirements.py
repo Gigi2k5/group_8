8f3d1c56387ae35e4fc62d47aa26868b191baf51
CheficathOlouchessi <136633823+CheficathOlouchessi@users.noreply.github.com>
2024-06-28 13:43:23 +0100

    Update Requirements.py

--- Requirements.py
+++ Requirements.py
@@ -11,25 +11,3 @@
 # Importation
 # import matplotlib.pyplot as plt
 
-# Cette ligne lit le fichier CSV 'Housing.csv' et stocke les données dans un DataFrame pandas appelé 'df'. 
-# df = pd.read_csv('Housing.csv')
-
-# Afficher les premières lignes du dataset pour un aperçu des données
-# df.head()
-
-# Création d'un histogramme pour la colonne 'bedrooms'
-# df['bedrooms'].plot(kind='hist')
-# Cette ligne crée un histogramme pour visualiser la distribution des valeurs dans la colonne 'bedrooms' du DataFrame.
-# Affichage du graphique avec la ligne 
-#plt.show()
-
-# Création d'un graphique de dispersion pour les colonnes 'area' et 'price'
-# plt.scatter(df['area'], df['price'])
-# Cette ligne crée un graphique de dispersion avec 'area' sur l'axe des x et 'price' sur l'axe des y pour visualiser leur relation.
-
-# Ajout d'un titre au graphique
-# plt.title('Graphique de dispersion: Area vs Price')
-
-# Cette ligne affiche le graphique créé par matplotib 
-# plt.show()
-
