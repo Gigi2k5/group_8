77ee2bf511e09f14a806f7878ccaed32b775a6c5
Kristilaourou <136489239+Kristilaourou@users.noreply.github.com>
2024-06-28 14:14:23 +0100

    Update Requirements.py

--- Requirements.py
+++ Requirements.py
@@ -1,13 +1,5 @@
-
-# pandas est un package pour la manipulation et l'analyse des données
-# Pour installer pandas, on a utilisé la commande:
 pip install pandas
-# Pour l'importer:
-import pandas as pd
-
-# matplotlib est un package pour la création de graphiques et de visualisations de données
-# Pour installer matplotlib, on a utilisé la commande:
 pip install matplotlib
-# Importation
-import matplotlib.pyplot as plt
+pip install diffusers["torch"] transformers
+
 
