1998e1626612f39aaa250c05aa03bd461c1fcbb6
CheficathOlouchessi <136633823+CheficathOlouchessi@users.noreply.github.com>
2024-06-28 13:55:59 +0100

    Update Requirements.py

--- Requirements.py
+++ Requirements.py
@@ -1,13 +1,13 @@
 
 # pandas est un package pour la manipulation et l'analyse des données
 # Pour installer pandas, on a utilisé la commande:
-# !pip install pandas
+pip install pandas
 # Pour l'importer:
-# import pandas as pd
+import pandas as pd
 
 # matplotlib est un package pour la création de graphiques et de visualisations de données
 # Pour installer matplotlib, on a utilisé la commande:
-# !pip install matplotlib
+pip install matplotlib
 # Importation
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
